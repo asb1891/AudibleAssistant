92e83e2758fe1422b3e21f137b0e2b13d210c535
asb1891 <137960726+asb1891@users.noreply.github.com>
2023-12-04 20:18:05 -0500

    fixed ResposnseComponent

--- my-react-app/src/ResponseComponent.js
+++ my-react-app/src/ResponseComponent.js
@@ -1,98 +1,50 @@
+import React, { useState, useEffect } from "react";
+import './index.css';
 
-import React, { useState, useEffect } from 'react';
+function ResponseComponent({ prompt, message }) {
+  const [ws, setWs] = useState(null);
+  const [responseData, setResponseData] = useState([]);
 
-function ResponseComponent({ prompt,  message , responseData }) {
-    // const [responseData, setResponseData] = useState([]);
-    const [ws, setWs] = useState(null);
+  useEffect(() => {
+    const newWs = new WebSocket('ws://localhost:6789');
 
-    useEffect(() => {
-        const newWs = new WebSocket('ws://localhost:6789');
-    
-        newWs.onopen = () => {
-            console.log('WebSocket Connected');
-            newWs.send("get_response");
-        };
-
-        newWs.onerror = (error) => {
-            console.error('WebSocket error:', error);
-        };
-
-        setWs(newWs);
-
-        return () => newWs.close();
-    }, []);
-    const chatBoxStyle = {
-        background: 'linear-gradient(0deg, #1E2C3D 0%, #3E364E 100%)',
-        margin: '100px auto',
-        maxWidth: '500px',
-        height: '650px',
-        boxShadow: '0 5px 30px rgba(0, 0, 0, 0.3)',
-        overflow: 'auto', // Changed to make the chatbox scrollable
-        position: 'relative',
-        transition: 'all 0.3s cubic-bezier(0.25, 0.5, 0.5, 0.9)',
-        background: '#DFDFEF', 
-        fontFamily: 'Helvetica Neue',
-        fontSize: '1.5em',
-        color: '#414141',
-        lineHeight: '2em',
-        padding: '10px' // Optional, for some spacing inside the chatbox
-    };
-    
-
-    const chatBubbleStyle = { // Size of the tail, adjust as needed
-        position: 'relative',
-        backgroundColor: 'rgba(32,178,170, 0.4)', // White with transparency
-        padding: '10px 15px', // Space inside the bubble
-        borderRadius: '20px', // Rounded corners
-        maxWidth: '70%', // Maximum width of a bubble
-        margin: '10px', // Space around each bubble
-        wordWrap: 'break-word',
-        textAlign: 'left',
-        fontSize: '16px',
-        lineHeight: '1.5',
-        marginLeft: 'auto',
-        marginRight: '0' // To ensure text breaks properly inside the bubble
-    };
-    const responseStyle = {
-        backgroundColor: 'rgba(188,143,143, 0.4)', // White with transparency
-        padding: '10px 15px', // Space inside the bubble
-        borderRadius: '20px', // Rounded corners
-        maxWidth: '70%', // Maximum width of a bubble
-        margin: '10px', // Space around each bubble
-        wordWrap: 'break-word',
-        textAlign: 'left',
-        fontSize: '16px',
-        lineHeight: '1.5',
-        marginLeft: '0',
-        marginRight: 'auto'
-    }
-
-    const chatHeaderStyle = {
-        backgroundColor: 'rgba(255, 255, 255, 0.4)',
-        fontStyle: 'italic',
-        padding: '10px 15px',
-        borderRadius: '20px',
-        maxWidth: '70%',
-        margin: '10px',
-        wordWrap: 'break-word',
+    newWs.onopen = () => {
+        console.log('WebSocket Connected');
+        newWs.send("get_response");
     };
 
-    return (
-        <div style={chatBoxStyle}>
-            <h2 style={chatHeaderStyle}>OpenAI Chat</h2>
-            {/* Render each prompt and response in separate bubbles */}
-            {responseData.length > 0 ? (
-            responseData.map(({ prompt, response }, index) => (
-                <React.Fragment key={index}>
-                    {prompt && <div style={chatBubbleStyle}>{prompt}</div>}
-                    {response && <div style={responseStyle}>{response}</div>}
-                </React.Fragment>
-                ))
-            ) : (
-                <div>Please Ask a Question!</div>
-            )}
-        </div>
-    );
-            }
+    newWs.onmessage = (event) => {
+        console.log("Message from server:", event.data);
+        const [prompt, response] = event.data.split('\n'); // Assuming the format "PROMPT: ... \n RESPONSE: ..."
+        setResponseData(prevData => [...prevData, { prompt, response }]);
+    };
+
+    newWs.onerror = (error) => {
+        console.error('WebSocket error:', error);
+    };
+
+    setWs(newWs);
+
+    return () => newWs.close();
+
+  }, []);
+
+  return (
+    <div className="chat-box">
+      <h2 className="chat-header">Audible Assistant</h2>
+      {/* Render each prompt and response in separate bubbles */}
+      {responseData.length > 0 ? (
+        responseData.map(({ prompt, response }, index) => (
+          <React.Fragment key={index}>
+            {prompt && <div className="chat-bubble">{prompt}</div>}
+            {response && <div className="response-style">{response}</div>}
+          </React.Fragment>
+        ))
+      ) : (
+        <div>Ask Away!</div>
+      )}
+    </div>
+  );
+}
 
 export default ResponseComponent;
