22bee1d69f0e9b8a69d99cf19a9a66e508943c2f
asb1891 <137960726+asb1891@users.noreply.github.com>
2023-12-04 20:20:25 -0500

    changes stable v 1

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import RecordingComponent from './RecordingComponent';
-import ResponseComponent from './ResponseComponent';
-import LoginComponent from './LoginComponent';
-import LogoutComponent from './LogoutComponent';
-import { useAuth0 } from '@auth0/auth0-react';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import RecordingComponent from "./RecordingComponent";
+import ResponseComponent from "./ResponseComponent";
+import LoginComponent from "./LoginComponent";
+import LogoutComponent from "./LogoutComponent";
+import { useAuth0 } from "@auth0/auth0-react";
+import "./index.css";
 
 function App() {
   // State to store the WebSocket instance
@@ -14,32 +15,32 @@ function App() {
 
   const [prompt, setPrompt] = useState(null);
 
-  const [responseData, setResponseData] = useState([])
+  const [responseData, setResponseData] = useState([]);
 
   const { isAuthenticated } = useAuth0();
 
   useEffect(() => {
     // Create a new WebSocket connection when the component mounts
-    const newWs = new WebSocket('ws://localhost:6789');
+    const newWs = new WebSocket("ws://localhost:6789");
     // Event handler for when the WebSocket connection opens
     newWs.onopen = () => {
-      console.log('Connected to the server');
+      console.log("Connected to the server");
     };
-    newWs. onprompt = (event) => {
-      console.log('Prompt from server:', event.data);
+    newWs.onprompt = (event) => {
+      console.log("Prompt from server:", event.data);
       setPrompt(event.data);
     };
     // Event handler for receiving messages from the WebSocket server
     newWs.onmessage = (event) => {
-      console.log('App Console Log:', event.data); 
-      const [prompt, response] = event.data.split('\n'); // Assuming the format "PROMPT: ... \n RESPONSE: ..."
-        setResponseData(prevData => [...prevData, { prompt, response }]);
+      console.log("App Console Log:", event.data);
+      const [prompt, response] = event.data.split("\n"); // Assuming the format "PROMPT: ... \n RESPONSE: ..."
+      setResponseData((prevData) => [...prevData, { prompt, response }]);
       // Update the message state with the received message
       setMessage(event.data);
     };
     // Event handler for any WebSocket errors
     newWs.onerror = (error) => {
-      console.error('WebSocket error:', error);
+      console.error("WebSocket error:", error);
     };
     // Update the WebSocket state with the new WebSocket instance
     setWs(newWs);
@@ -50,70 +51,63 @@ function App() {
     };
   }, []); // Empty dependency array means this effect runs once on mount and never again
 
-  const backgroundStyle = {
-    backgroundColor: '#ffff'
-  };
-  // const headerStyle = {
-  //   backgroundColor: 'rgba(255, 255, 255, 0.5)', // White with transparency
-  //       padding: '10px 15px', // Space inside the bubble
-  //       borderRadius: '20px', // Rounded corners
-  //       maxWidth: '17%', // Maximum width of a bubble
-  //       margin: '0 auto', // Space around each bubble
-  //       wordWrap: 'break-word', 
-  //       textAlign: 'center', 
-  // }
-
-  const headerStyle = {
-    maxWidth: '100px',
-    height: 'auto',
-  };
-
   const startRecording = () => {
     // Check if the WebSocket instance exists and is in the OPEN state
     if (ws && ws.readyState === WebSocket.OPEN) {
-        
-        // Send a message to the WebSocket server to start recording
-        ws.send("start_recording");
+      // Send a message to the WebSocket server to start recording
+      ws.send("start_recording");
     } else {
-        // Log a message if the WebSocket is not connected
-        console.log("WebSocket is not connected.");
+      // Log a message if the WebSocket is not connected
+      console.log("WebSocket is not connected.");
     }
-};
+  };
+  return (
+    <div className={isAuthenticated ? "background-style" : "login-style"}>
+      {!isAuthenticated ? (
+        <main className="column">
+          <h1 className="h1-style">Welcome to Audible Assistant</h1>
+          <LoginComponent />
+          <h2 className="h2-style">
+            Clicking the 'Sign In' button will redirect you to a 3rd party
+            authentication system
+          </h2>
+        </main>
+      ) : (
+        <>
+          {/* <h1 className="header-style">Andrew's Chat</h1> */}
+          <LogoutComponent />
+          <RecordingComponent ws={ws} startRecording={startRecording} />
+          <ResponseComponent
+            responseData={responseData}
+            prompt={prompt}
+            message={message}
+          />
+        </>
+      )}
+    </div>
+  );
 
-
-return (
-  <div>
-    {!isAuthenticated ? (
-      // Show login component when not authenticated
-      <main className="column">
-        <h1>Auth0 Login</h1>
-        <LoginComponent />
-      </main>
-    ) : (
-      // Show main content when authenticated
-      <div style={backgroundStyle}>
-        {/* <h1 style={headerStyle}>Andrew's Chat</h1> */}
-        <LogoutComponent />
-        <RecordingComponent ws={ws} startRecording={startRecording} />
-        <ResponseComponent responseData={responseData} prompt={prompt} message={message} />
-      </div>
-    )}
-  </div>
-);
+  // return (
+  //   <div style = {loginStyle}>
+  //     {!isAuthenticated ? (
+  //       // Show login component when not authenticated
+  //       <main className="column">
+  //         <h1 style= {h1Style}>Welcome to Audible Assistant</h1>
+  //         <LoginComponent />
+  //         <h2 style={h2Style}>Clicking the 'Sign In' button will redirect you to a 3rd party authentication system</h2>
+  //       </main>
+  //     ) : (
+  //       // Show main content when authenticated
+  //       <div style={backgroundStyle}>
+  //         {/* <h1 style={headerStyle}>Andrew's Chat</h1> */}
+  //         <LogoutComponent />
+  //         <RecordingComponent ws={ws} startRecording={startRecording} />
+  //         <ResponseComponent responseData={responseData} prompt={prompt} message={message} />
+  //       </div>
+  //     )}
+  //   </div>
+  // );
+  // }
 }
 
 export default App;
-
-//   return (
-//     <div style = {backgroundStyle}>
-//       <h1 style = {headerStyle}>Andrew's Chat</h1>
-//       <RecordingComponent ws={ws} startRecording={startRecording} />
-//       <ResponseComponent responseData={responseData} prompt = {prompt} message={message}/> {/* Include the ResponseComponent */}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
--- my-react-app/src/LoginComponent.js
+++ my-react-app/src/LoginComponent.js
@@ -1,19 +1,56 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import './index.css';
 
 const LoginComponent = () => {
     const { loginWithRedirect, isAuthenticated } = useAuth0();
 
-    return (
-        !isAuthenticated && (
-            <button onClick={() => loginWithRedirect({
-                authorizationParams: {
-                  redirect_uri: window.location.origin
-                }
-              })}>
-                Sign in
-            </button>
-        )
-    );
-}
+  //   const containerStyle = {
+  //     display: 'flex',
+  //     justifyContent: 'center', // Centers the button horizontally
+  //     alignItems: 'center',     // Centers the button vertically
+  //     height: 'auto',         // Take up the full viewport height
+  // };
 
+  return (
+    !isAuthenticated && (
+      <div className="container-style">
+        <button onClick={() => loginWithRedirect({
+          authorizationParams: {
+            redirect_uri: window.location.origin
+          }
+        })}>
+          Sign in
+        </button>
+      </div>
+    )
+  );
+
+//   return (
+//       !isAuthenticated && (
+//           <div style={containerStyle}>
+//               <button onClick={() => loginWithRedirect({
+//                   authorizationParams: {
+//                     redirect_uri: window.location.origin
+//                   }
+//                 })}>
+//                   Sign in
+//               </button>
+//           </div>
+//       )
+//   );
+// }
+
+//     return (
+//         !isAuthenticated && (
+//             <button onClick={() => loginWithRedirect({
+//                 authorizationParams: {
+//                   redirect_uri: window.location.origin
+//                 }
+//               })}>
+//                 Sign in
+//             </button>
+//         )
+//     );
+// }
+      };
 export default LoginComponent;
